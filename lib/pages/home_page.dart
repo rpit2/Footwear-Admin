c235254b5653e01863c2ebf5a5cd003725d4f522
shubhangi970 <shubhangi.jadhav@rpitsolutions.com>
2024-07-13 12:09:12 -0400

    first

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Footwear Admin"),
+      ),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index){
+         return const ListTile(
+           title: Text("Title 1"),
+           subtitle: Text('price 100'),
+         );
+
+      }),
+    );
+  }
+}
